bb1cbcc62be5560fb7b27798cdb7aada42a1abc6
Tyoma Makeev <tyomamakeev@gmail.com>
2025-06-03 00:56:28 +0400

    Exit command

--- src/main.c
+++ src/main.c
@@ -1,12 +1,19 @@
 #include <fcntl.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <wchar.h>
 
-#ifdef _WIN32
+#if defined(_WIN32) || defined(_WIN64)
+
 #include <io.h>
+#define strtok_r strtok_s
+#define wcstok_r wcstok_s
+
 #else
+
 #include <locale.h>
+
 #endif
 
 #define INPUT_BUFFER_SIZE 1024
@@ -19,20 +26,34 @@ void s_print(const wchar_t* input) {
     wprintf(L"\n");
 }
 
-void s_read(wchar_t* input) {
+void s_read(wchar_t* input, size_t* input_length) {
     fgetws(input, INPUT_BUFFER_SIZE, stdin);
 
     const size_t input_buffer_end = wcscspn(input, L"\r\n");
     input[input_buffer_end] = 0;
 }
 
-void s_eval(const wchar_t* input, const size_t output_size, wchar_t* output) {
+void s_eval(wchar_t* input, const size_t input_length, wchar_t* output, const size_t output_size) {
+    wchar_t* context;
+    const wchar_t* token = wcstok_r(input, L" ", &context);
+
+    if (wcscmp(token, L"exit") == 0) {
+        token = wcstok_r(NULL, L" ", &context);
+
+        if (token == NULL) {
+            exit(0);
+        }
+
+        const long int exit_code = wcstol(token, NULL, 10);
+        exit(exit_code);
+    }
+
     swprintf(output, output_size, L"%ls: command not found", input);
 }
 
 int main(int argc, char* argv[]) {
 
-#ifdef _WIN32
+#if defined(_WIN32) || defined(_WIN64)
     _setmode(_fileno(stdout), _O_U16TEXT);
     _setmode(_fileno(stderr), _O_U16TEXT);
 #else
@@ -45,10 +66,12 @@ int main(int argc, char* argv[]) {
     wchar_t input_buffer[INPUT_BUFFER_SIZE];
     wchar_t output_buffer[OUTPUT_BUFFER_SIZE];
 
+    size_t input_length = 0;
+
     while (keep_running) {
         wprintf(L"$ ");
-        s_read(input_buffer);
-        s_eval(input_buffer, OUTPUT_BUFFER_SIZE, output_buffer);
+        s_read(input_buffer, &input_length);
+        s_eval(input_buffer, input_length, output_buffer, OUTPUT_BUFFER_SIZE);
         s_print(output_buffer);
     }
 }
