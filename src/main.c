d55bc1ad6293e9a80350c7b53dc53a1f17ba28cc
Tyoma Makeev <tyomamakeev@gmail.com>
2025-06-06 00:02:21 +0400

    Run programs

--- src/main.c
+++ src/main.c
@@ -18,9 +18,14 @@
 
 #endif
 
-#define INPUT_BUFFER_SIZE 1024
-#define OUTPUT_BUFFER_SIZE 1024
+#define INPUT_BUFFER_SIZE 4096
+#define OUTPUT_BUFFER_SIZE 4096
+#define REUSABLE_CHAR_BUFFER_SIZE 4096
+#define REUSABLE_WCHAR_BUFFER_SIZE 4096
 #define SYSPATH_MAX_SIZE 4096
+
+static char reusable_char_buffer[REUSABLE_CHAR_BUFFER_SIZE];
+static wchar_t reusable_wchar_buffer[REUSABLE_WCHAR_BUFFER_SIZE];
 
 static const char* syspath;
 
@@ -43,30 +48,7 @@ static const struct wstring_to_function_pair builtins[BUILTINS_LENGTH] = {{L"exi
                                                                           {L"echo", builtin_echo},
                                                                           {L"type", builtin_type}};
 
-void builtin_exit(wchar_t* input, wchar_t* output) {
-    if (input == NULL) {
-        exit(0);
-    }
-
-    const long int exit_code = wcstol(input, NULL, 10);
-    exit(exit_code);
-}
-
-void builtin_echo(wchar_t* input, wchar_t* output) {
-    if (!input) {
-        input = L"";
-    }
-    swprintf(output, OUTPUT_BUFFER_SIZE, L"%ls", input);
-}
-
-void builtin_type(wchar_t* input, wchar_t* output) {
-    for (int i = 0; i < BUILTINS_LENGTH; i++) {
-        if (wcscmp(input, builtins[i].wstring) == 0) {
-            swprintf(output, OUTPUT_BUFFER_SIZE, L"%ls is a shell builtin", input);
-            return;
-        }
-    }
-
+void get_program_path_from_name(wchar_t* input, wchar_t* output) {
     if (syspath) {
         char path[SYSPATH_MAX_SIZE];
         sprintf(path, "%s", syspath);
@@ -83,7 +65,7 @@ void builtin_type(wchar_t* input, wchar_t* output) {
                     if (de->d_type == DT_REG) {
                         mbstowcs(output, de->d_name, OUTPUT_BUFFER_SIZE);
                         if (wcscmp(input, output) == 0) {
-                            swprintf(output, OUTPUT_BUFFER_SIZE, L"%ls is %s/%ls", input, dir, input);
+                            swprintf(output, OUTPUT_BUFFER_SIZE, L"%s/%ls", dir, input);
                             return;
                         }
                     }
@@ -92,13 +74,50 @@ void builtin_type(wchar_t* input, wchar_t* output) {
             dir = strtok_r(NULL, ":", &context);
         }
     }
+    output[0] = L'\0';
+}
 
-    swprintf(output, OUTPUT_BUFFER_SIZE, L"%ls: not found", input);
+void builtin_exit(wchar_t* input, wchar_t* output) {
+    if (input == NULL) {
+        exit(0);
+    }
+
+    const long int exit_code = wcstol(input, NULL, 10);
+    exit(exit_code);
+}
+
+void builtin_echo(wchar_t* input, wchar_t* output) {
+    if (!input) {
+        swprintf(output, OUTPUT_BUFFER_SIZE, L"");
+        return;
+    }
+    swprintf(output, OUTPUT_BUFFER_SIZE, L"%ls\n", input);
+}
+
+void builtin_type(wchar_t* input, wchar_t* output) {
+    if (!input) {
+        return;
+    }
+
+    for (int i = 0; i < BUILTINS_LENGTH; i++) {
+        if (wcscmp(input, builtins[i].wstring) == 0) {
+            swprintf(output, OUTPUT_BUFFER_SIZE, L"%ls is a shell builtin\n", input);
+            return;
+        }
+    }
+
+    get_program_path_from_name(input, reusable_wchar_buffer);
+    if (reusable_wchar_buffer[0] != L'\0') {
+        swprintf(output, OUTPUT_BUFFER_SIZE, L"%ls is %ls\n", input, reusable_wchar_buffer);
+        return;
+    }
+
+    swprintf(output, OUTPUT_BUFFER_SIZE, L"%ls: not found\n", input);
 }
 
 void s_print(const wchar_t* input) {
     fputws(input, stdout);
-    fputws(L"\n", stdout);
+    // fputws(L"\n", stdout);
 }
 
 void s_read(wchar_t* input) {
@@ -121,11 +140,30 @@ void s_eval(wchar_t* input, wchar_t* output, const size_t output_size) {
         }
     }
 
+    get_program_path_from_name(input, reusable_wchar_buffer);
+    if (reusable_wchar_buffer[0] != L'\0') {
+        wcstombs(reusable_char_buffer, reusable_wchar_buffer, REUSABLE_CHAR_BUFFER_SIZE);
+        if (context && context[0] != L'\0') {
+            swprintf(reusable_wchar_buffer, REUSABLE_WCHAR_BUFFER_SIZE, L"%s %ls", reusable_char_buffer, context);
+            wcstombs(reusable_char_buffer, reusable_wchar_buffer, REUSABLE_CHAR_BUFFER_SIZE);
+        }
+        system(reusable_char_buffer);
+        return;
+    }
+
     swprintf(output, output_size, L"%ls: command not found", input);
 }
 
-int main(int argc, char* argv[]) {
+void clear_buffers(wchar_t* input, wchar_t* output) {
+    input[0] = L'\0';
+    output[0] = L'\0';
+    reusable_char_buffer[0] = '\0';
+    reusable_wchar_buffer[0] = L'\0';
+}
 
+int main(int argc, char* argv[]) {
+    // system("ls -la");
+    // exit(1);
 #if defined(_WIN32) || defined(_WIN64)
     _setmode(_fileno(stdout), _O_U16TEXT);
     _setmode(_fileno(stderr), _O_U16TEXT);
@@ -142,6 +180,7 @@ int main(int argc, char* argv[]) {
     syspath = getenv("PATH");
 
     while (keep_running) {
+        clear_buffers(input_buffer, output_buffer);
         wprintf(L"$ ");
         s_read(input_buffer);
         s_eval(input_buffer, output_buffer, OUTPUT_BUFFER_SIZE);
