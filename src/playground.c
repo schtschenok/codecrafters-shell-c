53d740ef02e918bdffe1f1a56601e94d3edd6241
Tyoma Makeev <tyomamakeev@gmail.com>
2025-09-26 19:21:30 +0400

    Remove ferror-limit and clean up commented-out flags in CMakeLists Fix playground.c: correct types, NULL keyword and pass-by-value for str_t funcs

--- src/playground.c
+++ src/playground.c
@@ -4,13 +4,13 @@
 int main(void) {
     arena_t arena = arena_make(1024);
 
-    const c* some_string = "oooooaaoooaaaoo";
+    const char* some_string = "oooooaaoooaaaoo";
     str_t string_from_cstr = str_from_cstr(&arena, some_string);
 
     i64 context = -1;
-    str_t token = { nullptr, 0, 0 };
-    while (str_tokenize(&string_from_cstr, 'o', &context, &token)) {
-        str_write(&token, stdout, true);
+    str_t token = { NULL, 0, 0 };
+    while (str_tokenize(string_from_cstr, 'o', &context, &token)) {
+        str_write(token, stdout, true);
         printf("Context: %ld\n", context);
         printf("+++++\n");
     }
